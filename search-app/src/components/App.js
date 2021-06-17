aa5b0cdb7142fece0653ef2329c4000f368cde1a
Nick <maniutin@gmail.com>
2021-06-17 13:26:01 -0400

    add .then to axios req

--- search-app/src/components/App.js
+++ search-app/src/components/App.js
@@ -5,12 +5,17 @@ import SearchBar from "./SearchBar";
 
 class App extends React.Component {
   onSearchSubmit(term) {
-    axios.get("https://api.unsplash.com/search/photos", {
-      params: { query: term },
-      headers: {
-        Authorization: "Client-ID Y8YJuWWLSBTeBYvapFEilky0WynQbU_eME-B7Vmf7XE",
-      },
-    });
+    axios
+      .get("https://api.unsplash.com/search/photos", {
+        params: { query: term },
+        headers: {
+          Authorization:
+            "Client-ID Y8YJuWWLSBTeBYvapFEilky0WynQbU_eME-B7Vmf7XE",
+        },
+      })
+      .then((res) => {
+        console.log(res.data.results);
+      });
   }
 
   render() {
