76c0092f09f78b62be83a3ab147304db16728098
Nick <maniutin@gmail.com>
2021-06-17 13:35:47 -0400

    bind onSearchSubmit callback with () =>

--- search-app/src/components/App.js
+++ search-app/src/components/App.js
@@ -4,24 +4,23 @@ import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    axios
-      .get("https://api.unsplash.com/search/photos", {
-        params: { query: term },
-        headers: {
-          Authorization:
-            "Client-ID Y8YJuWWLSBTeBYvapFEilky0WynQbU_eME-B7Vmf7XE",
-        },
-      })
-      .then((res) => {
-        console.log(res.data.results);
-      });
-  }
+  state = { images: [] };
+
+  onSearchSubmit = async (term) => {
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization: "Client-ID Y8YJuWWLSBTeBYvapFEilky0WynQbU_eME-B7Vmf7XE",
+      },
+    });
+    this.setState({ images: response.data.results });
+  };
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        Found: {this.state.images.length}
       </div>
     );
   }
